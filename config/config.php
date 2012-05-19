51c7cbaaeea3c018c2017a6079fa73cc6b191c7b
Mitaj <mitaj999@gmail.com>
2012-05-20 00:00:40 +0700

    Before

---
+++ config/config.php
@@ -0,0 +1,66 @@
+;<? exit(); ?>
+
+license = "cdhbcaddlh jkpimgpjrk mnvsuwsxxa c544bbb6 fibdhemlni nnmortkosv vqw4y23578 c89ceigbeg gggijohhmr jlkplprttv sb38879b78 7afgiljcel mkljnjhnmp ospxr9v6v4 97cdaa8hdd geagiffkih ismpqvnqtv v5v6v7vb78 aeegdgckhj kphqllgnfn lroxtyr7r2 vasa6e478c chgleggecf gnklfnmppp s4p2m8p6rb "
+
+[database]
+
+;Сервер базы данных
+db_server = "localhost"
+
+;Пользователь базы данных
+db_user = "root"
+
+;Пароль к базе
+db_password = ""
+
+;Имя базы
+db_name = "Simpla"
+
+;Префикс для таблиц
+db_prefix = s_;
+
+;Кодировка базы данных
+db_charset = UTF8;
+
+;Режим SQL
+db_sql_mode =;
+
+[php]
+error_reporting = E_ALL;
+php_charset = UTF8;
+php_locale_collate = ru_RU;
+php_locale_ctype = ru_RU;
+php_locale_monetary = ru_RU;
+php_locale_numeric = ru_RU;
+php_locale_time = ru_RU;
+
+logfile = admin/log/log.txt;
+
+[smarty]
+
+smarty_compile_check = true;
+smarty_caching = false;
+smarty_cache_lifetime = 0;
+smarty_debugging = false;
+ 
+[images]
+
+;Директория оригиналов изображений
+original_images_dir = files/originals/;
+
+;Директория миниатюр
+resized_images_dir = files/products/;
+
+;Изображения категорий
+categories_images_dir = files/categories/;
+
+;Изображения брендов
+brands_images_dir = files/brands/;
+
+;Файл изображения с водяным знаком
+watermark_file = simpla/files/watermark/watermark.png;
+
+[files]
+
+;Директория хранения цифровых товаров
+downloads_dir = files/downloads/;
---
+++ design/default/html/register.tpl
@@ -0,0 +1,26 @@
+{* Страница регистрации *}
+
+<h1>Регистрация</h1>
+
+{if $error}
+<div class="message_error">
+	{if $error == 'empty_name'}Введите имя
+	{elseif $error == 'empty_email'}Введите email
+	{elseif $error == 'empty_password'}Введите пароль
+	{elseif $error == 'user_exists'}Пользователь с таким email уже зарегистрирован
+	{else}{$error}{/if}
+</div>
+{/if}
+
+<form class="form register_form" method="post">
+	<label>Имя</label>
+	<input type="text" name="name" data-format=".+" data-notice="Введите имя" value="{$name|escape}" maxlength="255" />
+	
+	<label>Email</label>
+	<input type="text" name="email" data-format="email" data-notice="Введите email" value="{$email|escape}" maxlength="255" />
+
+    <label>Пароль</label>
+    <input type="password" name="password" data-format=".+" data-notice="Введите пароль" value="" />
+
+	<input type=submit class="button" name="register" value="Зарегистрироваться">
+</form>
